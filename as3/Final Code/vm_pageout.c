1909765eb3858dbaafc35760ad3b686a11775f67
AHmed Khatab <b-b@dr.com>
2018-04-22 19:04:01 +0200

    Update vm_pageout.c

--- as3/Final Code/vm_pageout.c
+++ as3/Final Code/vm_pageout.c
@@ -1847,7 +1847,6 @@ vm_pageout_init(void)
 	 * case paging behaviors with stale active LRU.
 	 */
 	if (vm_pageout_update_period == 0)
-		//vm_pageout_update_period = 600;
 		vm_pageout_update_period = 10;
 
 	/* XXX does not really belong here */
